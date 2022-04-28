b2ef906e9679bcb3afb6c3caecd5f6d967f87212
fangjian <fangjian@droi.com>
2022-04-28 13:48:30 +0800

    init springboot env

---
+++ src/test/java/com/maxxspace/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxxspace.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
